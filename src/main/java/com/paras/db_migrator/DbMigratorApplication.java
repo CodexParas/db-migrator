8682be26c901629d8a15a48a41ea5a0da01ad280
Paras Gupta <parasgupta8sep@gmail.com>
2025-01-24 20:49:26 +0530

    Initial commit

---
+++ src/main/java/com/paras/db_migrator/DbMigratorApplication.java
@@ -0,0 +1,13 @@
+package com.paras.db_migrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMigratorApplication.class, args);
+	}
+
+}
